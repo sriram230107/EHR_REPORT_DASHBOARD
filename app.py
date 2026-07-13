2f9052ea3d71902aac354f9fb62198bedb07c6ee
SRI RAM B <rsbb7703@gmail.com>
2026-07-13 19:43:43 +0530

    LOGOUT

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask import session
 
 app=Flask(__name__)
+app.secret_key="abc123"
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -15,6 +17,7 @@ def doctor_login():
         did=request.form["doctor_id"]
         password=request.form["doctor_password"]
         if username=="admin" and did=="d123" and password=="admin@@":
+            session["doctor"]=username
             return redirect(url_for("doctor_dashboard"))
         else:
             return "Incorrect Credentials"
@@ -22,9 +25,13 @@ def doctor_login():
         return render_template("doctor_login.html")
 @app.route("/doctor_dashboard")
 def doctor_dashboard():
-    return render_template("doctor_dashboard.html")
-# @app.route("/login")
-# def login():
-#     return "LET'S LOGIN..!"
+    if "doctor" in session:
+        return render_template("doctor_dashboard.html",username=session["doctor"])
+    else:
+        return redirect(url_for("doctor_login"))
+@app.route("/logout")
+def logout():
+    session.pop("doctor")
+    return redirect(url_for("doctor_login"))
 if __name__ =="__main__":
     app.run(debug=True)
